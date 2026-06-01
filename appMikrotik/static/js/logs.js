3f906299d7de87149a90103d669e64668191cd3d
Jose Rodriguez <1005.31432935.ucla@gmail,com>
2026-06-01 18:20:56 -0400

    avances detalle log

---
+++ appMikrotik/static/js/logs.js
@@ -0,0 +1,25 @@
+function mapearDatosLogs(event) {
+    var button = event.relatedTarget;
+        if (!button) return;
+
+        var nombreOperador = button.getAttribute('data-log-nombreOperador') || '';
+        var mensaje = button.getAttribute('data-log-mensaje') || '';
+        var modulo = button.getAttribute('data-log-modulo') || '';
+        var fecha = button.getAttribute('data-log-fecha') || '';
+        var error = button.getAttribute('data-log-error') || '';
+
+        var nombreInput = detalleModal.querySelector('#detalle-log-nombreOperador');
+        if (nombreInput) nombreInput.value = nombreOperador;
+
+        var mensajeInput = detalleModal.querySelector('#detalle-log-mensaje');
+        if (mensajeInput) mensajeInput.value = mensaje;
+
+        var moduloInput = detalleModal.querySelector('#detalle-log-modulo');
+        if (moduloInput) moduloInput.value = modulo;
+
+        var fechaInput = detalleModal.querySelector('#detalle-log-fecha');
+        if (fechaInput) fechaInput.value = fecha;
+
+        var errorInput = detalleModal.querySelector('#detalle-log-error');
+        if (errorInput) errorInput.value = error;
+}
